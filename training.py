676b2f80d8eb5ac2824f666c2c4f6625702816f6
pavel <psgaiduk@gmail.com>
2022-02-06 08:28:55 +0300

    add DialogFlow learning script

---
+++ training.py
@@ -0,0 +1,43 @@
+from google.cloud import dialogflow
+import json
+import os
+from dotenv import load_dotenv
+
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+if __name__ == '__main__':
+    with open('questions.json', encoding='utf-8') as file_intents:
+        intents = json.load(file_intents)
+    for intent_name, training in intents.items():
+        print(intent_name)
+        questions = training['questions']
+        answer = training['answer']
+        create_intent('careful-gasket-340217', intent_name, questions, [answer])
