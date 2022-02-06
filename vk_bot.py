c2eeaed4bd2e7cd5c341c19f298b0f114b4f50df
pavel <psgaiduk@gmail.com>
2022-02-06 08:47:25 +0300

    echobot for VK

--- vk_bot.py
+++ vk_bot.py
@@ -1,25 +1,28 @@
-import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+import vk_api as vk
+import random
 from dotenv import load_dotenv
 import os
 
-if __name__ == '__main__':
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
-    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-    if os.path.exists(dotenv_path):
-        load_dotenv(dotenv_path)
+token_vk = os.environ['VK_TOKEN']
 
-    token_vk = os.environ['VK_TOKEN']
 
-    vk_session = vk_api.VkApi(token=token_vk)
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000)
+    )
 
+
+if __name__ == "__main__":
+    vk_session = vk.VkApi(token=token_vk)
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
-
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
