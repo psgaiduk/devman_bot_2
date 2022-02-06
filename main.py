e7c91f7f165e89e9330735437faf8ac9ec05f8c0
pavel <psgaiduk@gmail.com>
2022-02-06 08:28:41 +0300

    add answer from DialogFlow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import telebot
 import os
+from google.cloud import dialogflow
+
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
@@ -11,12 +13,28 @@ token_telegram = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(token=token_telegram)
 
 
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=texts, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text
+
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     if message.text == '/start':
         text = 'Здравствуйте'
     else:
-        text = message.text
+        text = detect_intent_texts('careful-gasket-340217', message.chat.id, message.text, 'ru-RU')
     bot.send_message(message.chat.id, text)
 
 
