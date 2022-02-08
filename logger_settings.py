844b9d4848695473e494e247520bf9417001c38a
pavel <psgaiduk@gmail.com>
2022-02-08 19:55:08 +0300

    fix work logger settings

--- logger_settings.py
+++ logger_settings.py
@@ -3,26 +3,25 @@ import logging
 from dotenv import load_dotenv
 import os
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
-
-
-CHAT_ID_LOGGER = os.environ['CHAT_ID']
-TOKEN_TELEGRAM_LOGGER = os.environ['TOKEN_TELEGRAM_LOGGER']
-
-bot_logger = telebot.TeleBot(token=TOKEN_TELEGRAM_LOGGER)
-
 
 class BotHandler(logging.Handler):
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+
+    CHAT_ID_LOGGER = os.environ['CHAT_ID']
+    TOKEN_TELEGRAM_LOGGER = os.environ['TOKEN_TELEGRAM_LOGGER']
+
+    bot_logger = telebot.TeleBot(token=TOKEN_TELEGRAM_LOGGER)
+
     def __init__(self):
         logging.Handler.__init__(self)
 
     def emit(self, record):
         message = self.format(record)
-        bot_logger.send_message(
+        self.bot_logger.send_message(
             text=f'{message}',
-            chat_id=CHAT_ID_LOGGER)
+            chat_id=self.CHAT_ID_LOGGER)
 
 
 logger_config = {
